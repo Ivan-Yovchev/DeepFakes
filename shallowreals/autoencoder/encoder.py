ceeb8a6ce3f03eda538b1d56b07ff3f2b1b6240c
Ivan-Yovchev <yovchev23@gmail.com>
2020-02-03 13:33:07 +0100

    Added optional layer

--- shallowreals/autoencoder/encoder.py
+++ shallowreals/autoencoder/encoder.py
@@ -4,17 +4,21 @@ from tensorflow.keras.models import Sequential
 
 class Encoder(Layer):
     """Maps MNIST digits to a triplet"""
-    def __init__(self, input_shape=(28, 28, 1), lowmem=True, name='Encoder', **kwargs):
+    def __init__(self, input_shape=(64, 64, 3), lowmem=True, name='Encoder', **kwargs):
         super(Encoder, self).__init__(name=name, **kwargs)
 
         self._input_shape = input_shape
+        self._lowmem = lowmem
         self._encoder_dim = 512 if lowmem else 1024
 
-        self.encoder = Sequential([
+        self.encoder_conv = Sequential([
                 Input(shape=self._input_shape),
                 Conv2D(128, kernel_size=5, strides=2, padding="same"),
                 Conv2D(256, kernel_size=5, strides=2, padding="same"),
-                Conv2D(512, kernel_size=5, strides=2, padding="same"),
+                Conv2D(512, kernel_size=5, strides=2, padding="same")
+            ])
+
+        self.encoder_reshape = Sequential([
                 Flatten(),
                 Dense(self._encoder_dim),
                 Dense(4 * 4 * 1024),
@@ -23,11 +27,16 @@ class Encoder(Layer):
             ])
 
     def call(self, inputs):
-        return self.encoder(inputs)
+        conv = self.encoder_conv(inputs)
+
+        if not self._lowmem:
+            conv = Conv2D(1024, kernel_size=5, strides=2, padding="same")(conv)
+
+        return self.encoder_reshape(conv)
 
 if __name__ == "__main__":
     # test
-    x = tf.random.normal((1, 28, 28, 1))
-    layer = Encoder()
+    x = tf.random.normal((3, 64, 64, 3))
+    layer = Encoder(lowmem=False)
     y = layer(x)
     print(y)
